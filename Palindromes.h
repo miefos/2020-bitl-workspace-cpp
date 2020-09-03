e1ac07f86cc85239e3c6b6a738fdd21a323390dc
Martins <ciekurs.martins@gmail.com>
2020-09-03 10:32:12 +0300

    first commit

---
+++ Palindromes.h
@@ -0,0 +1,14 @@
+#include <string>
+
+namespace ds_course
+{
+    class Palindromes
+    {
+    public:
+    Palindromes(); 
+    bool isPalindrome(int number);
+    bool isPalindrome(std::string word);
+    };
+}
+
+
